4beb0421478e6e35342897c8188ac93e605db5c9
Gustavo Leyte-Vidal Chacón <glcdmx@gmail.com>
2022-09-16 08:53:54 -0400

    feat: add second mobile card

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const starsSelected = document.querySelector(
+	".section-rating__card-rating__buttons-container"
+);
+const submitButton = document.querySelector(
+	".section-rating__card-rating__submit-button"
+);
+const cardRating = document.querySelector(".section-rating__card-rating");
+const cardClassification = document.querySelector(
+	".section-rating__card-classification"
+);
+const selection = document.querySelector(
+	".section-rating__card-classification__selection"
+);
+
+let stars = 0;
+starsSelected.addEventListener("click", (evt) => {
+	stars = evt.target.innerText;
+});
+
+submitButton.addEventListener("click", (evt) => {
+	if (stars) {
+		cardRating.classList.add("inactive");
+		cardClassification.classList.remove("inactive");
+		selection.innerText = `You selected ${stars} out of 5`;
+	}
+});
